187c83e78c78d669348ae34f368b14d9a033002e
istvanbaroti9 <istvanbaroti9@gmail.com>
2018-09-23 08:28:21 +0200

    01. commit

---
+++ src/main/java/com/elsospring/HomeController.java
@@ -0,0 +1,13 @@
+package com.elsospring;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "Szia Gyula!";
+	}
+}
